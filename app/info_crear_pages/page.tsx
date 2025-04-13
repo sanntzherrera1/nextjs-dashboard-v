da3946f11469a5d5e828d71619deca2c8e3ba609
sanntzherrera1 <luis.herreraipc@gmail.com>
2025-04-13 13:52:14 -0300

    deleted info page

--- app/info_crear_pages/page.tsx
+++
@@ -1,9 +0,0 @@
-/*
-Así es como puedes crear diferentes páginas en Next.js: crea un nuevo segmento de ruta usando una carpeta y agrega un pagearchivo dentro de él.
-
-creo una carpeta dentro de app -> creo el nombre de mi paginas y dentro pongo page.tsx
-
-ejemplo el "dashboard"
-
-
-*/
